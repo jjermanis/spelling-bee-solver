674d7355f271ee5c9e74127ea4460c4cfe815814
John Jermanis <jermanis@yahoo.com>
2023-02-08 07:10:05 -0800

    Dictionary cleanup tools

--- spelling-bee-solver/WordFinder.cs
+++ spelling-bee-solver/WordFinder.cs
@@ -2,7 +2,18 @@
 
 internal class WordFinder
 {
+    // TODO add words containing E and R to sanity check
+
+    private enum WordStatus
+    {
+        HasQ,
+        HasS,
+        TooManyLetters,
+        Valid
+    }
+
     private const string WORDS_PATH = @"..\..\..\words.txt";
+    private const string VALID_WORDS_PATH = @"..\..\..\update.txt";
 
     private readonly IEnumerable<string> _words;
 
@@ -30,5 +41,77 @@ internal class WordFinder
                 result.Add(word);
         }
         return result;
+    }
+
+    public void CreateSaneWordsFile()
+    {
+        File.WriteAllLines(VALID_WORDS_PATH, ValidWords());
+    }
+
+    private IEnumerable<string> ValidWords()
+    {
+        foreach (var word in _words)
+            if (GetWordStatus(word) == WordStatus.Valid)
+                yield return word;
+    }
+
+    private static WordStatus GetWordStatus(string word)
+    {
+        if (word.Contains('q'))
+            return WordStatus.HasQ;
+
+        if (word.Contains('s'))
+            return WordStatus.HasS;
+
+        var distinct = new HashSet<char>();
+        foreach (var c in word)
+            distinct.Add(c);
+        if (distinct.Count > 7)
+            return WordStatus.TooManyLetters;
+
+        return WordStatus.Valid;
+
+    }
+
+    public void SanityCheck()
+    {
+        var qCount = 0;
+        var sCount = 0;
+        var distinctLetterCount = 0;
+        foreach (var word in _words)
+        {
+            var status = GetWordStatus(word);
+            switch (status)
+            {
+                case WordStatus.HasQ:
+                    qCount++;
+                    break;
+                case WordStatus.HasS:
+                    sCount++;
+                    break;
+                case WordStatus.TooManyLetters:
+                    distinctLetterCount++;
+                    break;
+                case WordStatus.Valid:
+                    break;
+                default:
+                    throw new Exception($"Unhandled case: {status}");
+            }
+        }
+        Console.WriteLine($"{qCount} words containing Q.");
+        Console.WriteLine($"{sCount} words containing S.");
+        Console.WriteLine($"{distinctLetterCount} words with more than 7 different letters.");
+    }
+
+    public void CommonWords()
+    {
+        foreach (var word in _words)
+        {
+            var distinct = new HashSet<char>();
+            foreach (var c in word)
+                distinct.Add(c);
+            if (distinct.Count <= 3)
+                Console.WriteLine(word);
+        }
     }
 }
